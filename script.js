b42581a4d1a2590cc835a010747251367165559a
CaelsProjects <caelbarnesschool@gmail.com>
2025-06-20 13:46:53 -0400

    modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// JavaScript goes here.
+let chartrait;
+
+let cyb = 0;
+let uh = 0;
+let eo = 0;
+let jad = 0;
+
+
+
+document.getElementById("cyb").addEventListener("click", function() {
+    cyb++;
+    document.getElementById().style.display = "block"
+    questions.document.querySelector.style.display = "none";
+});
