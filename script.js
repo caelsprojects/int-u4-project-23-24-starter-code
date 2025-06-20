17e0900a8015929a167b51cb74d60bb58b8911d1
CaelsProjects <caelbarnesschool@gmail.com>
2025-06-20 14:47:04 -0400

    modified:   script.js

--- script.js
+++ script.js
@@ -47,21 +47,28 @@ function showResult() {
 
     if (cyb > 0 && good > 0) {
         winner = "Raiden";
+        winnerImg = "https://imgs.search.brave.com/YtHXEkaGdNzrBgyneyEDpJ7yaooxr4Y1Q2XLIwtt5tg/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS50ZW5vci5jb20v/VWI2VVd4Sy1XbWdB/QUFBTS9yYWlkZW4t/bWV0YWwtZ2Vhci1t/ZXRhbC1nZWFyLXJp/c2luZy5naWY.gif"
     } else if (cyb > 0 && bad > 0) {
         winner = "V1";
         winnerImg = "https://imgs.search.brave.com/56daUMavJpyvNoWNnOcxo-fWFp0LEKmAMua8Ro4Ihso/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS50ZW5vci5jb20v/cDJtOVNYTG4wendB/QUFBTS91bHRyYWtp/bGwtdjEuZ2lm.gif";
     } else if (eo > 0 && good > 0) {
         winner = "Isabelle";
+        winnerImg = "https://imgs.search.brave.com/fgdyFOp1rOIkhp-MESIwquhHcs-VIH1fvUkFwsXZ4V0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzI5Lzkz/L2ZjLzI5OTNmY2Iz/ZTFmZDVhMzkzNTM4/NWFiYzM3YzUwNzFi/LmpwZw";
     } else if (eo > 0 && bad > 0) {
         winner = "Papyrus";
+        winnerImg = "https://imgs.search.brave.com/ke5uqqfNbJVGwrGL9T3wp_nyNiEqJqdOfOeZJcyY-F0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJjYXZlLmNv/bS93cC93cDM4MzQz/NjEuanBn";
     } else if (uh > 0 && good > 0) {
         winner = "Doom Guy";
+        winnerImg = "https://imgs.search.brave.com/kZW0DxcSUJ30NRUeI1rg8cxgkvkHi3dOM3bSRj4RSjM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLmlt/Z2ZsaXAuY29tLzQv/ODZodG0wLnBuZw";
     } else if (uh > 0 && bad > 0) {
         winner = "Kratos";
+        winnerImg = "https://imgs.search.brave.com/bhoecTEh03VeQpWMfCsFif_etQrEbvXEwwH5c94SwuI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS50ZW5vci5jb20v/eUloTXFOVVpaU2tB/QUFBTS9nb2Qtb2Yt/d2FyLWtyYXRvcy5n/aWY.gif";
     } else if (jad > 0 && good > 0) {
         winner = "Dante";
+        winnerImg = "https://imgs.search.brave.com/4wvgsOMupCKDZHM-Lf-Pnmk1LkmLFDeCVxazBH7YN7Y/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS50ZW5vci5jb20v/Zy1rQ19wYVVvck1B/QUFBTS9kYW50ZS1k/YW5jZS5naWY.gif";
     } else if (jad > 0 && bad > 0) {
         winner = "Vergil";
+        winnerImg = "https://imgs.search.brave.com/jm3-1j3h-TjkVRg3X2lkhBGvWFoa9EZJp21AZu8wRko/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzL2YxL2Fm/LzJmL2YxYWYyZmJm/NTI1OGI0ODgzN2Y2/NjZlNWM1NmJjNzli/LmdpZg.gif";
     } else {
         winner = "Unknown";
     }
@@ -77,6 +84,7 @@ function showResult() {
         resultDiv.innerHTML = `
             <div class="winner">
                 <h2>${winner} is the winner!</h2>
+                <img src="${winnerImg}" alt="${winner}">
             </div>
         `;
     }
